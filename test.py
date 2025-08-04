eebcf2881b2d92fdd9a62148e7ff35c43715c15f
hczech27 <h.czech@sewertronics.com>
2025-08-04 09:09:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ c = float(input("Podaj zmienną c:"))
 avg = (a+b+c)/3
 
 print("Twoja średnia wynosi: " , avg)
+
+print("Jest to program przyjmujący od użytkownika jedną liczbę, ma on sprawdzić czy liczba jest dodatnia, ujemna czy równa zero")
+d = float(input("Podaj zmienną d:"))
+
+if(d==0):
+    print("Liczba jest równa zero")
+elif(d>0):
+    print("Liczba jest dodatnia")
+else:
+    print("Liczba jest ujemna")
