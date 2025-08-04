ce209573899df3032241fc1a6c8090295b37f66b
hczech27 <h.czech@sewertronics.com>
2025-08-04 08:59:26 +0200

    Update test.py
    Pierwszy skrypt

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello")
+print("hello world")
+a = float(input("Podaj zmienną a:"))
+b = float(input("Podaj zmienną b:"))
+c = float(input("Podaj zmienną c:"))
+avg = (a+b+c)/3
+
+print("Twoja średnia wynosi: " , avg)
